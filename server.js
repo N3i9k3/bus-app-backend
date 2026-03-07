0fcdaff60af1beb4b16ed94dc9b721077504bf27
Pratiksha18D <pratikshadighore9@gmail.com>
2026-03-08 02:40:26 +0530

    Added real-time bus location update using Socket.IO and MySQL

--- server.js
+++ server.js
@@ -28,6 +28,51 @@ app.use("/api/buses", busRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    },
+});
+
+// socket event handling
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("locationUpdate", async (data) => {
+
+  console.log("Location received:", data);
+
+  const bus_id = 1;
+  const { lat, lng } = data;
+
+  try {
+
+    await db.execute(
+      `INSERT INTO bus_locations (bus_id, latitude, longitude)
+       VALUES (?, ?, ?)
+       ON DUPLICATE KEY UPDATE
+       latitude = VALUES(latitude),
+       longitude = VALUES(longitude)`,
+      [bus_id, lat, lng]
+    );
+
+    io.emit("busLocation", { lat, lng });
+
+  } catch (error) {
+    console.error("DB ERROR:", error);
+  }
+
+});
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
