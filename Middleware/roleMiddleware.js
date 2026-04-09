b98958d9a1f45c083394413f6f25fa440d9513f6
Himanshi <6himanshi7bakde@gmail.com>
2026-04-09 13:42:21 +0530

    All changes are done

--- Middleware/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-const checkRole = (role) => {
-  return (req, res, next) => {
-    if (req.user.role !== role) return res.status(403).json({ message: "Access Denied" });
-    next();
-  };
-};
-
-module.exports = checkRole;
