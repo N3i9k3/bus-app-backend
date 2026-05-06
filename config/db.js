ad6254880d467068ffa57719552e1709ffc0a354
Nikita Mehare <nikitamehare8@gmail.com>
2026-05-06 18:56:00 +0530

    fix: updated db.js to use .env, fixed schema with missing columns

--- config/db.js
+++ config/db.js
@@ -16,6 +16,23 @@
 
 
 
+//const mysql = require("mysql2");
+//require("dotenv").config();
+
+//const db = mysql.createPool({
+//  host: process.env.DB_HOST,
+//  user: process.env.DB_USER,
+//  password: process.env.DB_PASSWORD,
+//  database: process.env.DB_NAME,
+//  port: process.env.DB_PORT || 3306,
+//});
+
+//module.exports = db.promise();
+
+
+
+
+
 const mysql = require("mysql2");
 require("dotenv").config();
 
@@ -24,7 +41,10 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = db.promise();
