0fb7e433a880545dd334632cc70fea380a06a62e
Nikita Mehare <nikitamehare8@gmail.com>
2026-05-06 19:03:09 +0530

    fix: db.js with SSL support for Aiven MySQL

--- config/db.js
+++ config/db.js
@@ -1,38 +1,3 @@
-// require("dotenv").config();
-// const mysql = require("mysql2");
-
-// const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
-// module.exports = db.promise();
-
-
-
-
-
-
-
-//const mysql = require("mysql2");
-//require("dotenv").config();
-
-//const db = mysql.createPool({
-//  host: process.env.DB_HOST,
-//  user: process.env.DB_USER,
-//  password: process.env.DB_PASSWORD,
-//  database: process.env.DB_NAME,
-//  port: process.env.DB_PORT || 3306,
-//});
-
-//module.exports = db.promise();
-
-
-
-
-
 const mysql = require("mysql2");
 require("dotenv").config();
 
@@ -42,9 +7,7 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = db.promise();
