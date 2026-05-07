89dae5226b3d1ed67215e5e194ad9665f75e4605
Nikita Mehare <nikitamehare8@gmail.com>
2026-05-07 06:03:21 +0530

    fix: hardcode Aiven credentials to test connection

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: "mysql-bus-app-nikitamehare8-9ba0.i.aivencloud.com",
+  user: "avnadmin",
+  password: "AVNS_ytw49N4SgM3Szw82cjq",
+  database: "defaultdb",
+  port: 16037,
   ssl: { rejectUnauthorized: false }
 });
 
